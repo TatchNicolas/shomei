5ba6c8b8583c427aca592f91040202774da1db30
TatchNicolas <TatchNicolas@users.noreply.github.com>
2022-04-13 12:00:24 +0900

    Add AWS subcommand

---
+++ cmd/aws/aws.go
@@ -0,0 +1,38 @@
+package aws
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/spf13/cobra"
+)
+
+var Cmd = &cobra.Command{
+	Use:   "aws",
+	Short: "Print HTTP request header for AWS",
+	Long:  "Print Authorization header for AWS using default credentials",
+	Run:   AWS,
+}
+
+func AWS(cmd *cobra.Command, args []string) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		fmt.Printf("ERROR in LoadDefaultConfig: %s", err)
+	}
+	credentials, err := cfg.Credentials.Retrieve(context.TODO())
+	if err != nil {
+		fmt.Printf("ERROR in Credentials.Retrieve: %s", err)
+	}
+	req, err := http.NewRequest("GET", "", nil)
+	signer := v4.NewSigner()
+	signer.SignHTTP(context.TODO(), credentials, req, "hash", "lambda", cfg.Region, time.Now())
+	fmt.Println(req.Header)
+}
+
+func Init() {
+
+}
--- cmd/root.go
+++ cmd/root.go
@@ -7,10 +7,9 @@ package cmd
 import (
 	"os"
 
+	"github.com/TatchNicolas/shomei/cmd/aws"
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -46,6 +45,5 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(aws.Cmd)
 }
-
-
